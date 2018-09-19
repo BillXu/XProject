4dfca70f516e66a6b81e05d67dab173b72e007f3
HaodiMa <215609657@qq.com>
2018-09-19 14:32:37 +0800

    formit for version 1.5
    as name

--- Server/GMQMaJiangServer/src/main.cpp
+++ Server/GMQMaJiangServer/src/main.cpp
@@ -1,22 +1,29 @@
 #include "MJServer.h"
 #include "Application.h"
 #include "MJPlayerCard.h"
+#include "MQMJPlayerCard.h"
 
 #include "IMJPoker.h"
 void tempTest()
 {
-	MJPlayerCard peer;
-	peer.addDistributeCard(make_Card_Num(eCT_Tong,3));
-	peer.addDistributeCard(make_Card_Num(eCT_Tong, 3));
-	peer.addDistributeCard(make_Card_Num(eCT_Tong, 5));
-	peer.addDistributeCard(make_Card_Num(eCT_Tong, 5));
-	peer.addDistributeCard(make_Card_Num(eCT_Tong, 4));
+	MQMJPlayerCard peer;
+	peer.addDistributeCard(make_Card_Num(eCT_Tong, 6));
+	peer.addDistributeCard(make_Card_Num(eCT_Tong, 7));
+	peer.addDistributeCard(make_Card_Num(eCT_Tong, 7));
+	peer.addDistributeCard(make_Card_Num(eCT_Tong, 7));
+	peer.addDistributeCard(make_Card_Num(eCT_Tong, 8));
 
-	peer.addDistributeCard(make_Card_Num(eCT_Tong, 4));
-	peer.addDistributeCard(make_Card_Num(eCT_Tong, 6));
-	peer.addDistributeCard(make_Card_Num(eCT_Tong, 6));
+	peer.addDistributeCard(make_Card_Num(eCT_Wan, 4));
+	peer.addDistributeCard(make_Card_Num(eCT_Wan, 5));
+	peer.addDistributeCard(make_Card_Num(eCT_Wan, 6));
+
+	peer.addDistributeCard(make_Card_Num(eCT_Tiao, 4));
+	peer.addDistributeCard(make_Card_Num(eCT_Tiao, 4));
+	peer.addDistributeCard(make_Card_Num(eCT_Tiao, 7));
+	peer.addDistributeCard(make_Card_Num(eCT_Tiao, 8));
+	peer.addDistributeCard(make_Card_Num(eCT_Tiao, 9));
 	uint8_t nJiang = 0;
-	auto nRet = peer.isHoldCardCanHu(nJiang);
+	auto nRet = peer.canHuWitCard(make_Card_Num(eCT_Tong, 7));
 	if (nRet)
 	{
 		printf("ok hu");
